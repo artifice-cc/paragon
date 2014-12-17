c3c21ac15d17764681b32ac7c89824ce6cca4e78
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2014-12-17 09:01:31 -0500

    Changed project namespace.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paragon "0.1.0-SNAPSHOT"
+(defproject cc.artifice/paragon "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
