e1978c1522961f3a554983b47e4f3233e8994d15
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2015-04-11 07:55:41 -0400

    Added randomized tests analysis code.

---
+++ analysis/paragon-random.R
@@ -0,0 +1,58 @@
+
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(stringr)
+
+data.frame.wide <- function(x, vid, vmeasure, long_col)
+{
+  x$id <- c(1:nrow(x))
+  xmelt <- melt(x, c(c("id"), vid, c(long_col)), vmeasure)
+  xc <- dcast(xmelt, as.formula(paste(paste(c("id", vid), collapse="+"), "~", long_col)))
+  newcols <- as.vector(unique(x[[long_col]]))
+  tmpd1 <- data.frame(xc[!is.na(xc[[newcols[1]]]),vid])
+  return(cbind(tmpd1, lapply(newcols, function(c) {
+    tmpd2 <- data.frame(na.omit(xc[[c]]));
+    colnames(tmpd2) <- c(c);
+    return(tmpd2);
+  })))
+}
+
+row.measures.differ <- function(row, dx)
+{
+  return(any(sapply(6:ncol(dx), function(i) { row[i] != row[5] })))
+}
+
+analyze.measure <- function(d, measure, base_strat)
+{
+  dwide <- data.frame.wide(d, c("Case", "ChanceAnd", "ChanceSplit", "Inconsistent"), 
+                           measure, "Strategy")
+  dwide <- cbind(dwide[,1:5], lapply(colnames(dwide)[6:ncol(dwide)], function(col) {
+    dcol <- data.frame(x=(100.0*(dwide[,col] - dwide[,base_strat] + 1.0)/(dwide[,base_strat] + 1.0)))
+    colnames(dcol) <- c(paste("REL", col, sep=""))
+    dcol
+  }))
+  dsub <- dwide[(1:nrow(dwide))[sapply(1:nrow(dwide), function(x) { row.measures.differ(dwide[x,], dwide) })],]
+  dsubm <- melt(dsub, c("Case", "ChanceAnd", "ChanceSplit", "Inconsistent"))
+  print(head(dwide))
+  print(paste("mean of", base_strat, mean(dwide[,base_strat])))
+  print(lapply(unique(d$Strategy), function(strat) {
+    if(str_sub(strat, 0, 3) != "REL" && strat != base_strat)
+    {
+      list(strat,
+           paste("pct increase:", mean(dwide[,paste("REL", strat, sep="")])),
+           t.test(d[d$Strategy==strat,measure], d[d$Strategy==base_strat,measure], paired=TRUE))
+    }
+  }))
+  ggplot(dsubm) + geom_boxplot(aes(x=variable, y=value)) + # , fill=factor(Inconsistent)
+    #facet_grid(ChanceAnd ~ ChanceSplit, labeller=label_both) + labs(fill="Inconsistent") +
+    scale_x_discrete("Strategy") + scale_y_continuous(measure) + 
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+}
+
+d <- read.csv("paragon-random-abduce.csv")
+#analyze.measure(d, "ExplainedPct", "rand_rand")
+analyze.measure(d, "ChangePct", "rand_rand")
+#analyze.measure(d, "ChangePct", "rand")
+
+
