9b2d2ed86bb3e8f56fbfb61924f8fdab032dc10d
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2015-04-11 08:12:56 -0400

    Support turning on/off node/stroke labels in (visualize).
    Bumped version to 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/paragon "1.2.0"
+(defproject cc.artifice/paragon "1.3.0"
   :description "An implementation and expansion of Neil Tennant's belief contraction system documented in Changes of Mind (2012, Oxford Univ. Press)."
   :url "http://paragon.artifice.cc"
   :license {:name "Eclipse Public License"
--- src/paragon/core.clj
+++ src/paragon/core.clj
@@ -99,19 +99,24 @@
   (graph/incoming (:graph jg) stroke-or-node))
 
 (defnp visualize-dot
-  [jg]
+  [jg node-labels? stroke-labels?]
   (let [g (:graph jg)
         g-nodes (-> g
+                    (graphattr/add-attr-to-nodes :fillcolor :white (filter #(white? jg %) (nodes jg)))
                     (graphattr/add-attr-to-nodes :fillcolor :black (filter #(black? jg %) (nodes jg)))
-                    (graphattr/add-attr-to-nodes :fontcolor :white (filter #(black? jg %) (nodes jg))))
+                    (graphattr/add-attr-to-nodes :fontcolor :white (filter #(black? jg %) (nodes jg)))
+                    (graphattr/add-attr-to-nodes :fontcolor :black (filter #(white? jg %) (nodes jg))))
         g-strokes (-> g-nodes
                       (graphattr/add-attr-to-nodes :shape :plain (strokes jg))
                       (graphattr/add-attr-to-nodes :height 0.1 (strokes jg))
-                      (graphattr/add-attr-to-nodes :label "&nbsp;" (strokes jg))
+                      (graphattr/add-attr-to-nodes :fillcolor :white (filter #(white? jg %) (strokes jg)))
                       (graphattr/add-attr-to-nodes :fillcolor :black (filter #(black? jg %) (strokes jg)))
-                      (graphattr/add-attr-to-nodes :fillcolor :white (filter #(white? jg %) (strokes jg))))
-        g-node-labels (reduce (fn [g n] (graphattr/add-attr g n :label (jgstr n)))
-                              g-strokes (nodes jg))]
+                      (graphattr/add-attr-to-nodes :fontcolor :white (filter #(black? jg %) (strokes jg)))
+                      (graphattr/add-attr-to-nodes :fontcolor :black (filter #(white? jg %) (strokes jg))))
+        g-stroke-labels (reduce (fn [g s] (graphattr/add-attr g s :label (if stroke-labels? (jgstr s) "&nbsp;")))
+                                g-strokes (strokes jg))
+        g-node-labels (reduce (fn [g n] (graphattr/add-attr g n :label (if node-labels? (jgstr n) "&nbsp;")))
+                              g-stroke-labels (nodes jg))]
     ;; add bottom node properties (if bottom node exists)
     (-> g-node-labels
         (graphattr/add-attr :bottom :label "&perp;")
@@ -119,8 +124,9 @@
         (graphattr/add-attr :bottom :shape :none))))
 
 (defn visualize
-  [jg]
-  (graphio/view (visualize-dot jg) :node {:fillcolor :white :style :filled :fontname "sans"}))
+  [jg & {:keys [node-labels? stroke-labels?] :or {node-labels? true stroke-labels? false}}]
+  (graphio/view (visualize-dot jg node-labels? stroke-labels?)
+                :node {:fillcolor :white :style :filled :fontname "sans"}))
 
 (defn save-pdf
   [jg fname]
