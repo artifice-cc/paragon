ea8398666fe1f7fdeeb631efe89539e89e008dc1
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2015-09-01 09:43:32 -0400

    Another bugfix with building from a nested query
    Bumped version to 2.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/paragon "2.0.1"
+(defproject cc.artifice/paragon "2.0.2"
   :description "An implementation and expansion of Neil Tennant's belief contraction system documented in Changes of Mind (2012, Oxford Univ. Press)."
   :url "http://paragon.artifice.cc"
   :license {:name "Eclipse Public License"
